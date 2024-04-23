c4f227c46cfed27d60a99be30794d9a859eab338
P. H. Staats <144123448+patrick6018145@users.noreply.github.com>
2024-04-23 13:09:30 +0200

    Update question rendering in app.js and Questions module

--- embed/libs/app.js
+++ embed/libs/app.js
@@ -1,7 +1,9 @@
 import { Questions } from './modules/questions.js';
 
-// template literal question, answers, questionType(default = multiple choice)
-const vragen =
-    new Questions('Wat is de hoofdstad van Nederland?', ['Amsterdam', 'Rotterdam', 'Den Haag', 'Utrecht']);
+// template literal question, answers, questionType(default = multiple choice | open = text area)
+const vragen = [
+    new Questions('Wat is de hoofdstad van Nederland?', ['Amsterdam', 'Rotterdam', 'Den Haag', 'Utrecht']),
+    new Questions('Wat is de hoofdstad van ietsanders?', [], 'open')
+];
 
-vragen.appendQuestion();
+vragen[1].appendQuestion();
--- embed/libs/modules/questions.js
+++ embed/libs/modules/questions.js
@@ -2,11 +2,14 @@ const vraagContainer = document.getElementById('vraag');
 const antwoordenContainer = document.getElementById('antwoorden');
 
 
+let idCounter = 0;
+
 export class Questions {
     constructor(question, answers=null, questionType='default') {
         this.question = question;
         this.answers = answers;
         this.questionType = questionType;
+        this.id = `question-${idCounter++}`;
     }
 
     appendQuestion() {
@@ -16,15 +19,20 @@ export class Questions {
 
         if (this.questionType === 'default') {
             vraagContainer.innerHTML = this.question;
-            this.answers.forEach(answer => {
+            this.answers.forEach((answer, index) => {
                 const antwoord = document.createElement('a');
                 antwoord.innerHTML = answer;
                 antwoord.classList.add('antwoord');
+                antwoord.id = `${this.id}-answer-${index + 1}`;
                 antwoordenContainer.appendChild(antwoord);
             });
         }
 
-        vraagContainer.innerHTML = this.question;
-        const textArea = document.createElement('textarea');
+        if (this.questionType === 'open') {
+            vraagContainer.innerHTML = this.question;
+            const textArea = document.createElement('textarea');
+            textArea.classList.add('antwoord-textarea');
+            antwoordenContainer.appendChild(textArea);
+        }
     }
 }
