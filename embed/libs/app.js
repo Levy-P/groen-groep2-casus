635d1fd7e72d63a7585110a3791924fcfa2e975e
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-04-24 23:08:03 +0200

    overhaul questions system with a root object instead of an array

--- embed/libs/app.js
+++ embed/libs/app.js
@@ -1,10 +1,19 @@
-import { Questions } from './modules/questions.js';
+import { Question } from './modules/questions.js';
 
 // template literal question, answers, questionType(default = multiple choice | open = text area)
-export const vragen = [
-    new Questions('Wat is de hoofdstad van Nederland?', ['Amsterdam', 'Rotterdam', 'Den Haag', 'Utrecht']),
-    new Questions('Wat is de hoofdstad van ietsanders?', [], 'open')
-];
+export const scanResults = [];
+export const vragen = {
+    0: new Question(2, 'Test', [0,1250]),
+
+    test: new Question(1, 'Wat is de hoofdstad van Nederland?', [
+        {text:'Amsterdam',value:'amsterdam',event:'open'},
+        {text:'Rotterdam',value:'rotterdam',event:0},
+        {text:'Den Haag',value:'den_haag',event:0},
+        {text:'Utrecht',value:'utrecht',event:0}
+    ]),
+    
+    
+};
 
 // vragen[0].showNextQuestion(0);
-Questions.startQuestions();
+Question.startQuestions();
--- embed/libs/modules/questions.js
+++ embed/libs/modules/questions.js
@@ -1,85 +1,91 @@
 import { vragen } from "../app.js";
+import { scanResults } from "../app.js";
 
 const vraagContainer = document.getElementById('vraag');
 const antwoordenContainer = document.getElementById('antwoorden');
 
-
 let idCounter = 0;
 
-export class Questions {
-    constructor(question, answers=null, questionType='default') {
+export class Question {
+    constructor(questionType=0, question, answers=null) {
         this.question = question;
         this.answers = answers;
         this.questionType = questionType;
         this.id = `question-${idCounter++}`;
     }
 
-    static getNextQuestion(currentIndex) {
-        if (currentIndex < vragen.length - 1) {
-            currentIndex++;
-            return vragen[currentIndex];
-        } else {
-            return null;
-        }
+    static getNextQuestion(answerEvent) {
+        return answerEvent in vragen ? vragen[answerEvent] : null;
     }
 
     appendQuestion() {
-        if (this.answers === null && this.questionType === 'default') {
-            throw new Error('Answers are required for this question type');
+        switch(this.questionType) {
+            // * Slider Vraag
+            case 2: {
+                const sliderContainer = document.createElement('div'),
+                      sliderInput = document.createElement('input');
+
+                sliderInput.classList.add('slider');
+                sliderInput.classList.add('antwoord');
+                sliderInput.setAttribute('type','range');
+
+                let mean = Math.round((this.answers[0]+this.answers[1])/2);
+                sliderInput.setAttribute('min',this.answers[0]);
+                sliderInput.setAttribute('max',this.answers[1]);
+                sliderInput.setAttribute('value',mean);
+
+                sliderContainer.classList.add('slider-container');
+                sliderContainer.appendChild(sliderInput);
+
+                antwoordenContainer.appendChild(sliderContainer);
+                break;
+            }
+
+            // * Meerkeuze Vraag
+            case 1: {
+                if (this.answers === null) throw new Error('Answers are required for this question type');
+
+                vraagContainer.innerHTML = this.question;
+                this.answers.forEach((answer, index) => {
+                    const antwoord = document.createElement('a');
+
+                    antwoord.classList.add('antwoord');
+                    antwoord.innerHTML = answer.text;
+                    antwoord.id = `${this.id}-answer-${index + 1}`;
+                    antwoord.setAttribute('evt',answer.event)
+                    antwoordenContainer.appendChild(antwoord);
+                });
+                break;
+            }
+
+            default: throw new Error(`Questiontype '${this.questionType}' is not recognized`)
         }
-
-        if (this.questionType === 'default') {
-            vraagContainer.innerHTML = this.question;
-            this.answers.forEach((answer, index) => {
-                const antwoord = document.createElement('a');
-                antwoord.innerHTML = answer;
-                antwoord.classList.add('antwoord');
-                antwoord.id = `${this.id}-answer-${index + 1}`;
-                antwoordenContainer.appendChild(antwoord);
-            });
-        }
-
-        if (this.questionType === 'open') {
-            vraagContainer.innerHTML = this.question;
-            const textArea = document.createElement('textarea');
-            textArea.placeholder = 'Typ hier je antwoord';
-            textArea.id = `${this.id}-textarea`;
-            textArea.classList.add('antwoord-textarea');
-            antwoordenContainer.appendChild(textArea);
-
-            const submitBtn = document.createElement('a');
-            submitBtn.innerHTML = 'Submit';
-            submitBtn.classList.add('antwoord');
-            submitBtn.id = `${this.id}-answer-submit`;
-            antwoordenContainer.appendChild(submitBtn);
-        }
-    }
-
-    showNextQuestion(currentIndex) {
-        vraagContainer.innerHTML = '';
-        antwoordenContainer.innerHTML = '';
-
-        const nextQuestion = Questions.getNextQuestion(currentIndex);
-        if (nextQuestion) {
-            nextQuestion.appendQuestion();
-        } else {
-            console.log('No more questions');
-        }
-        // const nextQuestion = getNextQuestion();
-        // nextQuestion.appendQuestion();
-    }
-
-    static startQuestions() {
-        vragen[0].appendQuestion();
 
         const answerElements = document.querySelectorAll('.antwoord');
         answerElements.forEach(answer => {
             answer.addEventListener('click', (e) => {
-                const answerId = e.target.id;
-                const answerIndex = parseInt(answerId.split('-')[2]);
-                const currentQuestionIndex = parseInt(answerId.split('-')[1]);
-                vragen[currentQuestionIndex].showNextQuestion(currentQuestionIndex);
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+
+                const answerEvt = answer.getAttribute('evt');
+                if (answerEvt in vragen) vragen[answerEvt].showNextQuestion();
+                else this.clearQuestions();
             });
         });
+    }
+
+    showNextQuestion() {
+        scanResults.push('test');
+        console.log(scanResults);
+        this.clearQuestions()
+        this.appendQuestion();
+    }
+
+    clearQuestions() {
+        vraagContainer.innerHTML = antwoordenContainer.innerHTML = '';
+    }
+
+    static startQuestions() {
+        vragen[0].appendQuestion();
     }
 }
