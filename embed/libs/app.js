af550230d08e453ef5e9b9977a46d8446d0c06a3
P. H. Staats <144123448+patrick6018145@users.noreply.github.com>
2024-04-25 10:05:22 +0200

    Update question rendering logic in app.js and Questions module

--- embed/libs/app.js
+++ embed/libs/app.js
@@ -1,6 +1,7 @@
 import { Question } from './modules/questions.js';
 
 // template literal question, answers, questionType(default = multiple choice | open = text area)
+// questionType: 0 = multiple choice, 1 = multiple choice with answers, 2 = slider
 export const scanResults = [];
 export const vragen = {
     0: new Question(2, 'Hoeveel FTE', {
@@ -31,13 +32,97 @@ export const vragen = {
         }
     ]),
 
-    reden_gegroeid: new Question,
+    reden_gegroeid: new Question(1, 'Dit komt door', [
+        {
+            text:'Is uit zijn jasje gegroeid!',
+            value:'gegroeid',
+            event:'open'
+        },
+        {
+            text:'Kan naar een hoger niveau',
+            value:'hoger',
+            event:0
+        },
+        {
+            text:'Heeft te maken met (nieuwe) wet en regelgeving',
+            value:'',
+            event:0
+        },
+        {
+            text:'Is goed zoals het is',
+            value:'utrecht',
+            event:0
+        }
+    ]),
 
-    willen_graag: new Question,
+    hoger_niveau: new Question(1, 'Wij willen graag', [
+        {
+            text:'Is uit zijn jasje gegroeid!',
+            value:'gegroeid',
+            event:'open'
+        },
+        {
+            text:'Kan naar een hoger niveau',
+            value:'hoger',
+            event:0
+        },
+        {
+            text:'Heeft te maken met (nieuwe) wet en regelgeving',
+            value:'',
+            event:0
+        },
+        {
+            text:'Is goed zoals het is',
+            value:'utrecht',
+            event:0
+        }
+    ]),
 
-    willen_dit: new Question,
+    heeft_te_maken_met: new Question(1, 'En willen dit', [
+        {
+            text:'Is uit zijn jasje gegroeid!',
+            value:'gegroeid',
+            event:'open'
+        },
+        {
+            text:'Kan naar een hoger niveau',
+            value:'hoger',
+            event:0
+        },
+        {
+            text:'Heeft te maken met (nieuwe) wet en regelgeving',
+            value:'',
+            event:0
+        },
+        {
+            text:'Is goed zoals het is',
+            value:'utrecht',
+            event:0
+        }
+    ]),
 
-    is_goed_want: new Question
+    is_goed_zoal_is: new Question(1, 'Want: (vink aan wat van toepassing)', [  // TODO: * Meerkeuze Vraag met zin invullen
+        {
+            text:'Is uit zijn jasje gegroeid!',
+            value:'gegroeid',
+            event:'open'
+        },
+        {
+            text:'Kan naar een hoger niveau',
+            value:'hoger',
+            event:0
+        },
+        {
+            text:'Heeft te maken met (nieuwe) wet en regelgeving',
+            value:'',
+            event:0
+        },
+        {
+            text:'Is goed zoals het is',
+            value:'utrecht',
+            event:0
+        }
+    ]),
 };
 
 Question.startQuestions();
