073d89e43107b4a70bf8a37bc127186d2f5af870
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-04-25 12:57:59 +0200

    add assets

--- embed/libs/visuals.js
+++ embed/libs/visuals.js
@@ -1,6 +1,18 @@
 const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
+const img = {
+    train: new Image(),
+    track: new Image()
+}
+
+for (svg in img) {
+    img[svg] = document.getElementById('svg-'+svg);
+}
+
 
 setInterval(() => {
-    c.width = window.innerWidth;
+    w_width = c.width = window.innerWidth;
+    w_height = window.innerHeight
+
+    ctx.drawImage(img.train,0,0,50,50);
 }, 50);
